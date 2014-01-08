a9cc9afa57854355ba6ecf66769e6495656cfbaf
Feifei HANG <l.daniel.hung@gmail.com>
2014-01-08 21:04:35 +0000

    Added build.js to be used for building final products by RequireJS Optimizer

---
+++ template/build.js
@@ -0,0 +1,10 @@
+{
+    baseUrl: 'js/dev/',
+    dir: 'js/dist/',
+    mainConfigFile: 'config.js',
+    removeCombined: true,
+    fileExclusionRegExp: /^\./,
+    modules: [
+        {name: 'main'}
+    ]
+}
