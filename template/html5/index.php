c77819da67be5bc11f99a990f5e7e0036e32b510
Feifei HANG <l.daniel.hung@gmail.com>
2014-01-09 23:35:50 +0000

    Fixed the redundant jQuery including in index.php

--- template/html5/index.php
+++ template/html5/index.php
@@ -44,7 +44,6 @@ else {
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
         <script src=\"js/plugins.js\"></script>
 
 <?php
@@ -57,7 +56,7 @@ if ($ribs == 'Y' || $ribs == 'y') {
 }
 
 if ($requirejs == 'Y' || $requirejs == 'y') {
-    echo "\n\t\t<!-- change data-main into \"dist/js/..\" before release -->\n";
+    echo "\n\t\t<!-- change data-main into \"dist/js/..\" before release -->";
     echo "\n\t\t<script data-main=\"js/built/main.js\" src=\"js/vendor/require.min.js\"></script>\n";
 }
 
