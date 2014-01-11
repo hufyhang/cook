51f35e5a60af31593082efb31e0b62a1cedce401
Feifei HANG <l.daniel.hung@gmail.com>
2014-01-11 15:42:12 +0000

    Added Routie.mim.js into fetching list

--- template/html5/index.php
+++ template/html5/index.php
@@ -18,19 +18,19 @@ $requirejs = $_GET['requirejs'];
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
         <link rel="stylesheet" href="vendor/css/normalize.css">
-
 <?php
 if ($bootstrap == 'Y' || $bootstrap == 'y') {
-    echo "\n\t\t<link rel=\"stylesheet\" href=\"vendor/css/bootstrap.min.css\">\n";
-    echo "\n\t\t<link rel=\"stylesheet\" href=\"vendor/css/bootstrap-theme.min.css\">\n";
+    echo "\n\t\t<link rel=\"stylesheet\" href=\"vendor/css/bootstrap.min.css\">";
+    echo "\n\t\t<link rel=\"stylesheet\" href=\"vendor/css/bootstrap-theme.min.css\">";
 }
 if ($requirejs == 'Y' || $requirejs == 'y') {
-    echo "\n\t\t<link rel=\"stylesheet\" href=\"dist/css/style.min.css\">\n";
+    echo "\n\t\t<link rel=\"stylesheet\" href=\"dist/css/style.min.css\">";
 }
 else {
-    echo "\n\t\t<link rel=\"stylesheet\" href=\"css/main.css\">\n";
+    echo "\n\t\t<link rel=\"stylesheet\" href=\"css/main.css\">";
 }
 ?>
+
         <script src="vendor/js/modernizr.js"></script>
     </head>
     <body>
@@ -44,25 +44,26 @@ else {
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="vendor/js/jquery.min.js"><\/script>')</script>
         <script src="vendor/js/plugins.js"></script>
-
 <?php
 if ($bootstrap == 'Y' || $bootstrap == 'y') {
-    echo "\n\t\t<script src=\"vendor/js/bootstrap.min.js\"></script>\n";
+    echo "\n\t\t<script src=\"vendor/js/bootstrap.min.js\"></script>";
 }
 
 if ($ribs == 'Y' || $ribs == 'y') {
-    echo "\n\t\t<script src=\"vendor/js/ribs.min.js\"></script>\n";
+    echo "\n\t\t<script src=\"vendor/js/ribs.min.js\"></script>";
+    echo "\n\t\t<script src=\"vendor/js/routie.min.js\"></script>";
 }
 
 if ($requirejs == 'Y' || $requirejs == 'y') {
     echo "\n\t\t<!-- change data-main into \"dist/js/..\" before release -->";
-    echo "\n\t\t<script data-main=\"js/built/main.js\" src=\"vendor/js/require.min.js\"></script>\n";
+    echo "\n\t\t<script data-main=\"js/built/main.js\" src=\"vendor/js/require.min.js\"></script>";
 }
 
 if ($requirejs != 'Y' && $requirejs != 'y') {
-    echo "\n\t\t<script src=\"js/main.min.js\"></script>\n";
+    echo "\n\t\t<script src=\"js/main.min.js\"></script>";
 }
 ?>
+
     </body>
 </html>
 
